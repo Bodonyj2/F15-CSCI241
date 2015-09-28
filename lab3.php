88ebbf429b71f6446c05d51ed657fbbc20fe4a73
Jacob Bodony <bodonyj2@winthrop.edu>
2015-09-28 11:20:43 -0400

    added lab3

--- lab3.php
+++ lab3.php
@@ -5,8 +5,8 @@
 	</head>
 	<body>
 		
-
-		<pre><?php 
+	
+	<?php 
     
     if($_SERVER['REQUEST_METHOD'] == 'POST')
 	{
@@ -18,30 +18,50 @@
 		$priceItem2 = ($_POST["priceItem2"]);
 		$priceItem3 = ($_POST["priceItem3"]);
 		$priceItem4 = ($_POST["priceItem4"]);	
-		$includeTax = ($_POST["includeTax"]);
+		
 		
 			if(isset($_POST["includeTax"]))
      			{
-     			echo "Invoice Item 1:" + $invoiceItem1 + "Price" + (($priceItem1 * .07) + $priceItem1);
-	  			echo "Invoice Item 2:" + $invoiceItem1 + "Price" + (($priceItem2 * .07) + $priceItem2);
-	  			echo "Invoice Item 3:" + $invoiceItem1 + "Price" + (($priceItem3 * .07) + $priceItem3);
-	  			echo "Invoice Item 4:" + $invoiceItem1 + "Price" + (($priceItem4 * .07) + $priceItem4);
+     			$subTotal = ($priceItem1 + $priceItem2 + $priceItem3 + $priceItem4);
+					if(isset($_POST["includeTax"]))
+					{
+					$total = (($subTotal * .07) + $subTotal);
+					}	
+					else 
+					{
+					$total = $subTotal;	
+					}
+ 
+     			echo "Invoice Item 1: " . $invoiceItem1 . "<br>" . " Price: $" . number_format(($priceItem1 * .07) + $priceItem1, 2) . "<br>" . "<br>";
+	  			echo "Invoice Item 2: " . $invoiceItem2 . "<br>" . " Price: $" . number_format(($priceItem2 * .07) + $priceItem2, 2) . "<br>" . "<br>";
+	  			echo "Invoice Item 3: " . $invoiceItem3 . "<br>" . " Price: $" . number_format(($priceItem3 * .07) + $priceItem3, 2) . "<br>" . "<br>";
+	  			echo "Invoice Item 4: " . $invoiceItem4 . "<br>" . " Price: $" . number_format(($priceItem4 * .07) + $priceItem4, 2) . "<br>" . "<br>";
+				echo "Subtotal: $" . number_format($subTotal, 2) . "<br>";
+				echo "Total: $" . number_format($total, 2);
 				}
 			else 
 				{
-				echo "Invoice Item 1:" + $invoiceItem1 + "Price" + $priceItem1;
-	  			echo "Invoice Item 2:" + $invoiceItem1 + "Price" + $priceItem2;
-	  			echo "Invoice Item 3:" + $invoiceItem1 + "Price" + $priceItem3;
-	  			echo "Invoice Item 4:" + $invoiceItem1 + "Price" + $priceItem4;
-				}	
+				echo "Invoice Item 1: " . $invoiceItem1 . "<br>" . " Price: $" . $priceItem1 . "<br>" . "<br>";
+	  			echo "Invoice Item 2: " . $invoiceItem2 . "<br>" . " Price: $" . $priceItem2 . "<br>" . "<br>";
+	  			echo "Invoice Item 3: " . $invoiceItem3 . "<br>" . " Price: $" . $priceItem3 . "<br>" . "<br>";
+	  			echo "Invoice Item 4: " . $invoiceItem4 . "<br>" . " Price: $" . $priceItem4 . "<br>" . "<br>";
+				}
+		}		
+	else		
+		{			
     ?>
-    </pre>	
-	<form method="post" action="lab3.php" name="form">
+  	
+		<form method="post" action="lab3.php" name="form">
 		Invoice Item 1: <input type="text" name="invoiceItem1"> Price: <input type="text" name="priceItem1"><br>
 		Invoice Item 2: <input type="text" name="invoiceItem2"> Price: <input type="text" name="priceItem2"><br>
 		Invoice Item 3: <input type="text" name="invoiceItem3"> Price: <input type="text" name="priceItem3"><br>
 		Invoice Item 4: <input type="text" name="invoiceItem4"> Price: <input type="text" name="priceItem4"><br>
 		Include Tax? <input type="checkbox" name="includeTax">
 		<input type="submit" value="Submit">
-	</form>
+		</form>
 	<?php
+		}
+	?>
+	</body>
+	</html>
+	
